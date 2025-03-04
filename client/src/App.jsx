0f4cbd145e3a86dbcef1ba3634951fff46e0a5c4
my-react-spaapp <chayanid.sin@dome.tu.ac.th>
2025-03-04 11:21:23 +0700

    git add . git commit -m "Refactor app to use Context API for global state management" git push origin main

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,8 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import ProductList from "./components/ProductList";
-import { useState } from "react";
+import { ProductProvider } from "./components/ProductContext";
 import ProductList from "./components/ProductList";
 import AddProduct from "./components/AddProduct";
 
